69227593d7909e357bddcf836ae82e57ed73a996
thedatanex <naresh.bt2k2@gmail.com>
2025-10-25 10:22:25 -0700

    Update settings.py

--- amortization_app/settings.py
+++ amortization_app/settings.py
@@ -4,8 +4,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-DEBUG = True
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ['syzygy_amortization-app-dj.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
